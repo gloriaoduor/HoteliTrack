3dfb88a8440c0ea125cb1060ecf1571d51dd55d6
gloria <gloriaoduor@gmail.com>
2022-10-11 16:56:40 +0300

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Guest.create("fname": "Gloria", "sname": "Oduor", "gender": "female", "id_no": "123456", "is_citizen": true )
+Guest.create("fname": "Aphonso", "sname": "Kip", "gender": "male", "id_no": "654321", "is_citizen": true )
+Guest.create("fname": "Rosita", "sname": "Paloma", "gender": "female", "id_no": "994876", "is_citizen": false )
+Guest.create("fname": "Daniel", "sname": "Spielburgh", "gender": "male", "id_no": "447556", "is_citizen": false )
+Guest.create("fname": "Grace", "sname": "Mercy", "gender": "female", "id_no": "423456", "is_citizen": true )
+
+Room.create("room_type": "Deluxe", "price": 15000, "admin_id": "", "guest_id": " ")
+Room.create("room_type": "Single", "price": 5000, "admin_id": "", "guest_id": " ")
+Room.create("room_type": "Pent", "price": 25000, "admin_id": "", "guest_id": " ")
+Room.create("room_type": "Double", "price": 10000 , "admin_id": "", "guest_id": " ")
+Room.create("room_type": "Suite", "price": 30000, "admin_id": "", "guest_id": " ")
+Room.create("room_type": "Deluxe", "price": 15000, "admin_id": "", "guest_id": " ")
+Room.create("room_type": "Single", "price": 5000, "admin_id": "", "guest_id": " ")
+Room.create("room_type": "Pent", "price": 25000, "admin_id": "", "guest_id": " ")
+Room.create("room_type": "Double", "price": 10000 , "admin_id": "", "guest_id": " ")
+Room.create("room_type": "Suite", "price": 30000, "admin_id": "", "guest_id": " ")
+
+Employee.create("name": "", "admin_id": 1, "guest_id": 1)
+Employee.create("name": "", "admin_id": 1, "guest_id": 1) #create different ids for guest/emp
+Employee.create("name": "", "admin_id": 1, "guest_id": 1)
+Employee.create("name": "", "admin_id": 1, "guest_id": 1)
+Employee.create("name": "", "admin_id": 1, "guest_id": 1)
+
+
+
+
