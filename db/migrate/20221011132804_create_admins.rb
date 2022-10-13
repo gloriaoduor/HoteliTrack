39fac405ca1e18021cd2349d4b10f74d8afab1fd
gloria oduor <gloriaoduor@gmail.com>
2022-10-13 16:28:47 +0300

    edit db fields via migrations

--- db/migrate/20221011132804_create_admins.rb
+++ db/migrate/20221011132804_create_admins.rb
@@ -1,6 +1,8 @@
 class CreateAdmins < ActiveRecord::Migration[6.1]
   def change
     create_table :admins do |t|
+      t.string :email
+      t.string :password
       t.string :name 
       t.string :location
       t.boolean :is_admin
--- db/migrate/20221011132923_create_guests.rb
+++ db/migrate/20221011132923_create_guests.rb
@@ -3,9 +3,12 @@ class CreateGuests < ActiveRecord::Migration[6.1]
     create_table :guests do |t|
       t.string :fname 
       t.string :sname
-      t.string :gender
       t.integer :id_no
       t.string :nationality
+      t.string :gender
+      t.string :email
+      t.integer :tel_no
+      
 
       t.timestamps
     end
