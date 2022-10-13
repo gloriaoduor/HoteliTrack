14c27045b37819edcf11992060b5b3e91ac7c713
gloria oduor <gloriaoduor@gmail.com>
2022-10-13 16:10:14 +0300

    new guest form

--- client/src/components/AddGuest.js
+++ client/src/components/AddGuest.js
@@ -1,46 +1,76 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+
 
 function AddGuest(){
 
+    function onChangeValue(e){
+        console.log(e.target.value)
+    }
+    // onValueChange(e) {
+    //     this.setState({
+    //       selectedOption: e.target.value
+    //     });
+    //   }
+
     return (
-        <div className="row d-flex justify-content-center align-items-center h-60">
-            <div className="col-md-6">
-            <form className="login-form">
-          {/* <!-- name input --> */}
-            <div className="form-outline mb-4">
-            {/* <label className="form-label" htmlFor="form3Example3">First Name </label> */}
-            <input type="name" id="form3Example3" className="form-control form-control-sm"
-            placeholder="First Name" />
-            </div> 
-
-            {/* second name  */}
-            <div className="form-outline mb-4">
-            <input type="name" id="form3Example3" className="form-control form-control-sm"
-            placeholder="Second Name" />
-            </div> 
-
-            {/* Identification number */}
-            <div className="form-outline mb-4">
-            <input type="name" id="form3Example3" className="form-control form-control-sm"
-            placeholder="ID No." />
-            </div>
-
-            {/* Gender */}
-            <div className="form-outline mb-4 ">
-                <input type="radio" value="Male" name="gender" /> Male
-                <input type="radio" value="Female" name="gender" /> Female
-            </div>
-
-            {/* is_a_citizen */}
-            <div className="form-check mb-0">
-                <input className="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
-                <label className="form-check-label" htmlFor="form2Example3">Citizen of Kenya?</label>
-            </div>
-
-            </form>
-            </div>
+        <form className="p-3 mt-3 w-100 bg-light">
+            {/* names */}
+            <div class="col-md-12">
+                <div className="row m-1">
+                    <div className="col-lg-6">
+                        <input type="text" className="form-control form-control-md" placeholder="First Name" />
+                    </div>
+                    <div className="col-lg-6">
+                        <input type="text" className="form-control form-control-md" placeholder="Second Name" />
+                    </div>
+                </div>
             
-        </div>
+                {/*  Nationality and Id number*/}
+                <div className="row m-1">
+                    <div className="col-lg-6">
+                        <input type="text" class="form-control" placeholder="Nationality"/>
+                    </div>
+                    <div className="col-lg-6">
+                        <input type="text" class="form-control" placeholder="ID no."/>
+                    </div>
+                </div>
+                {/* Contacts */}
+                <div className="row m-1">
+                    <div className="col-lg-6">
+                        <input type="text" class="form-control" placeholder="Email"/>
+                    </div>
+                    <div className="col-lg-6">
+                        <input type="text" class="form-control" placeholder="Tel. No"/>
+                    </div>
+                </div>
+                {/* Gender */}
+                <div className="row m-1" onChange={onChangeValue}>              {/* use function onValueChange instead */}
+                    <div className="form-check form-check-inline ">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="Male"/>
+                        <label class="form-check-label" for="inlineRadio1">Male</label>
+                    </div>
+                    <div className="form-check form-check-inline col-lg-6">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="Female"/>
+                        <label class="form-check-label" for="inlineRadio2">Female</label>
+                    </div>
+
+                </div>
+                {/* Room allocation */}
+                <div className="row m-1">
+                    <div className="col-lg-6">
+                        <input type="text" class="form-control" placeholder="Room Type"/>
+                    </div>
+                    <div className="col-lg-6">
+                        <input type="text" class="form-control" placeholder="Room no."/>
+                    </div>
+                </div>
+                <div className="text-center text-lg-start mt-4 pt-2">
+                    <Link to="/" type="button" className="btn btn-primary btn-md">Add Guest</Link>
+                </div>
+            </div>
+        </form>
+       
     );
 }
 export default AddGuest;
