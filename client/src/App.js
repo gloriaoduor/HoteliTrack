d9604c4cf53f7aac56e83d9bc70ad5d4b052d0a2
gloria oduor <gloriaoduor@gmail.com>
2023-02-17 16:09:24 +0300

    react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
+import { Link } from 'react-router-dom';
+
 
 
 function App() {
+
+  // const navigate = useNavigate();
+
   return (
     <div className=''>
       <div className='App container-fluid'>
         <div className='row'>
-          <div className='col-6'>
+          <div className='col-sm-6'>
             <div className='landing'>
-              <img className='img' alt="logo" src="https://cdn-icons-png.flaticon.com/512/58/58291.png" />
+              <img className='img img-fluid' alt="logo" src="https://cdn-icons-png.flaticon.com/512/58/58291.png" />
               <h1 className='Header pt-2 font-italic'>HOTELiTRACK </h1> 
              
             </div>
           </div>
-          <div className='infocol col-6 '>
+          <div className='infocol col-sm-6 '>
             <p className='info'>Join us today for: <br />
               <ul>
                 <li>Lorem ipsum dolor sit amet,consectetur adipiscing elit.</li>
@@ -22,7 +27,9 @@ function App() {
                 <li>Praesent bibendum ante ac neque sodales</li>
               </ul>
             </p>
-            <button className='button btn  rounded mb-4'>Get Started...</button>
+            <Link to="/dashboard"><button className='button btn  rounded mb-4'
+            >
+              Get Started...</button></Link>
           </div>
         </div>
 
